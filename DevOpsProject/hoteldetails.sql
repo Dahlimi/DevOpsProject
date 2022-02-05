f4f761a810d58185a891bbbcf3c3ba3d64e3beb7
armanrahman <2004498H@student.tp.edu.sg>
2022-02-06 00:54:01 +0800

    Upload of Hotel Details SQL database!!

---
+++ DevOpsProject/hoteldetails.sql
@@ -0,0 +1,58 @@
+-- phpMyAdmin SQL Dump
+-- version 5.1.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Feb 05, 2022 at 05:51 PM
+-- Server version: 10.4.21-MariaDB
+-- PHP Version: 8.0.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `hoteldetails`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `hoteldetails`
+--
+
+CREATE TABLE `hoteldetails` (
+  `name` varchar(20) NOT NULL,
+  `address` varchar(255) NOT NULL,
+  `pricing` varchar(20) NOT NULL,
+  `star` varchar(20) NOT NULL,
+  `image` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `hoteldetails`
+--
+
+INSERT INTO `hoteldetails` (`name`, `address`, `pricing`, `star`, `image`) VALUES
+('V Social Singapore', 'Tampenis', '5-Star', '999++', 'heh');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `hoteldetails`
+--
+ALTER TABLE `hoteldetails`
+  ADD PRIMARY KEY (`name`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
